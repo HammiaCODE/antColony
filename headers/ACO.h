9f2c79e5d5861cd271c9c221ec7d619e2e1fdf76
HammiaCODE <hanescper@gmail.com>
2025-11-30 20:59:12 -0600

    Testing probability code

--- headers/ACO.h
+++ headers/ACO.h
@@ -10,7 +10,36 @@
 #include <string>
 #include <stdexcept>
 
-int ACO_TSP(const vector<City>& cities, int alpha,int beta, int rho,int num_cities){
+
+
+float probabilities(const vector<City>& cities, double alpha,double beta, double rho,int num_cities){
+
+    vector<double> probTabu;
+    vector<double> probConstruct;
+
+
+    // distance matrix, time and space complexity O(n^2)
+    vector<vector<double>> distances = DistGraph(cities);
+
+    //build edges, O(n^2)
+    vector<Edge> edges = BuildEdges(distances, num_cities);
+    
+    double desirability = 1/num_cities;
+    double pheromone;
+    
+    double probs = ((pheromone)*alpha)*(desirability*(beta));
+    double sumProbs,construct;
+    probTabu.push_back(probs);
+
+    for(int j=0;j<num_cities;j++){
+        sumProbs= probTabu[j]+probTabu[j+1];
+        construct=probs/sumProbs;
+        probConstruct.push_back(construct);
+    }
+}
+
+
+int ACO_TSP(const vector<City>& cities, double alpha, double beta, double rho,int num_cities){
     // distance matrix, time and space complexity O(n^2)
     vector<vector<double>> distances = DistGraph(cities);
 
