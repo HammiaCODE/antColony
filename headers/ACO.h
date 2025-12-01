24918f5a2f4f95455b2091fe25f1c6fae8a7fb94
HammiaCODE <hanescper@gmail.com>
2025-11-30 20:38:04 -0600

    Edits to ACO.h

--- headers/ACO.h
+++ headers/ACO.h
@@ -10,4 +10,13 @@
 #include <string>
 #include <stdexcept>
 
+int ACO_TSP(const vector<City>& cities, int alpha,int beta, int rho,int num_cities){
+    // distance matrix, time and space complexity O(n^2)
+    vector<vector<double>> distances = DistGraph(cities);
+
+    //build edges, O(n^2)
+    vector<Edge> edges = BuildEdges(distances, num_cities);
+
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <iomanip>
 #include "./headers/tsp_loader.h"
+#include "./headers/ACO.h"
 
 
 using namespace std;
@@ -20,6 +21,7 @@ int main(){
     cout << "Ant Colony Optimization Algorithm for TSP" << endl; 
 
     int num_cities, iterations;
+    int beta, alpha, rho;
     cout << "How many cities? ";
     cin >> num_cities;
 
@@ -27,12 +29,45 @@ int main(){
     cout << "How many iterations? ";
     cin >> iterations;
 
+    cout<< endl;
+    cout << "Beta (β): ";
+    cin >> beta;
+
+    cout<< endl;
+    cout << "Alpha (a): ";
+    cin >> alpha;
+
+    cout<< endl;
+    cout << "Rho (P): ";
+    cin >> rho;
+
     if(num_cities > total_cities.size()){
         num_cities = total_cities.size();
     }
 
+    // funcion random de n ciudades para guardarlas en cities no repetidas
+    vector<int> unique_n(total_cities.size());
+    for(int i = 0; i < total_cities.size(); ++i){
+        unique_n[i] = i;
+    }
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    for(int i = 0; i < num_cities; ++i){
+        uniform_int_distribution<> dis(i, total_cities.size() - 1);
+        int j = dis(gen);
+        swap(unique_n[i], unique_n[j]);
+    }
+
+    vector<City> cities;
+    for (int i = 0; i < num_cities; ++i) {
+        cities.push_back(total_cities[unique_n[i]]);
+    }
+
     for(int i=0;i<iterations;i++){
-        
+        //cout << ACO_TSP(cities,alpha,beta,rho)<<endl;
+
     }
 
     return 0;
